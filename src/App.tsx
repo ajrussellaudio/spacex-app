692f0cc1595e8a688916aad81b464d91cd242df1
Alan Russell <ajrussellaudio@gmail.com>
2024-01-31 13:25:38 +0000

    Add grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Launches } from "./components/Launches";
+import { Grid } from "@mui/material";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Launches />
+        </Grid>
+        <Grid item xs={8}>
+          <div>🚧 Launch info 🚧</div>
+        </Grid>
+      </Grid>
+    </QueryClientProvider>
   );
 }
 
